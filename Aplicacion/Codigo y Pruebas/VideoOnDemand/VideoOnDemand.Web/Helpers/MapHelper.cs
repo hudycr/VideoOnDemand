a3b0b11d81621133892f0ed855964407ea5301a6
hudycr <36305519+hudycr@users.noreply.github.com>
2018-07-31 20:43:25 -0500

    Se agrega solucion base
    Solucion base

---
+++ Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Web/Helpers/MapHelper.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoOnDemand.Web.Helpers
+{
+    public class MapHelper
+    {
+        internal static IMapper mapper;
+
+        static MapHelper()
+        {
+            var config = new MapperConfiguration(x => {
+
+
+               });
+            mapper = config.CreateMapper();
+        }
+
+        public static T Map<T>(object source)
+        {
+            return mapper.Map<T>(source);
+        }
+    }
+}
